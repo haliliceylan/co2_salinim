9c6b12dd4f00f8e4ed259b04130de3727e80f779
Hande Hayran <handehayrann@gmail.com>
2020-04-30 19:47:57 +0000

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CO2 Emissions',
       theme: ThemeData(
         fontFamily: 'Arial',
         primaryColor: Color(0xFF8BC34A),
